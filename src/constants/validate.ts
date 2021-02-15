416ea047e2a023604f235179f888932e5b4d0aaf
d0hyeon <ehgus6887@gmail.com>
2021-02-15 23:22:21 +0900

    test commit

--- src/constants/validate.ts
+++ src/constants/validate.ts
@@ -12,5 +12,5 @@ export const ID_RULES: Validate[] = [
 
 export const PASSWORD_RULES: Validate[] = [
   {required: true, message: '비밀번호를 입력해주세요.' },
-  {pattern: REGEXP_PASSWORD, message: '영문자, 숫자를 포함하여 8글자 이상 입력해주세요.'}
+  {pattern: REGEXP_PASSWORD, message: '영문자, 숫자를 포함하여 8글자 이상 입력해주세요.'}, 
 ]
